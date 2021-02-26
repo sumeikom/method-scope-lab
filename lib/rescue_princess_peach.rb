65e376f07b63aad99546cd1bda53ffa70143a095
sumeikom <sumeikom@gmail.com>
2021-02-26 00:15:20 -0500

    commit tree c9805aaa1ea2e0bfc402033f2bb84004e206e1e7

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -3,6 +3,11 @@ def rescue_princess_peach
   puts "Hooray! Mario has rescued Princess Peach."
 end
 
+def catch_phrase(phrase)
+  phrase = "It's-a me, Mario!"
+  puts phrase
+  end
+
 ## Comment the below back in to see method scope in action!
 
 # rescue_princess_peach
